ba109df9694ddcd3b1a592aaa4afe1796a058dd5
Aleksandr <aleksandr@example.com>
2026-05-19 15:20:18 +0600

    debug: log Cloudinary credentials

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -5,18 +5,21 @@
 
 const cloudinary = require('cloudinary').v2;
 
+console.log('[Cloudinary] CLOUDINARY_URL:', process.env.CLOUDINARY_URL ? 'set' : 'NOT SET');
+console.log('[Cloudinary] CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('[Cloudinary] API_KEY:', process.env.CLOUDINARY_API_KEY ? 'set' : 'NOT SET');
+
 if (process.env.CLOUDINARY_URL) {
-  cloudinary.config({
-    cloudinary_url: process.env.CLOUDINARY_URL,
-    secure: true
-  });
-} else {
+  cloudinary.config(process.env.CLOUDINARY_URL);
+} else if (process.env.CLOUDINARY_CLOUD_NAME && process.env.CLOUDINARY_API_KEY) {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
   });
+} else {
+  console.warn('[Cloudinary] WARNING: No CLOUDINARY credentials configured!');
 }
 
 module.exports = cloudinary;

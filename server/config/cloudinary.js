c0f0c96e1a4ebdeceeb42096965142748ad8109d
Aleksandr <aleksandr@example.com>
2026-05-19 15:16:44 +0600

    fix: support CLOUDINARY_URL env var

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -5,11 +5,18 @@
 
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true
-});
+if (process.env.CLOUDINARY_URL) {
+  cloudinary.config({
+    cloudinary_url: process.env.CLOUDINARY_URL,
+    secure: true
+  });
+} else {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+  });
+}
 
 module.exports = cloudinary;

c67e2b2ef21549f5f9cb9a22fc8463dada00588c
Aleksandr <aleksandr@example.com>
2026-07-17 21:16:23 +0600

    perf(mobile): add flutter_displaymode for high refresh rate on Xiaomi

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 
 import 'src/app.dart';
 import 'src/core/prefs/love_prefs.dart';
@@ -6,5 +7,8 @@ import 'src/core/prefs/love_prefs.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LovePrefs.instance.init();
+  try {
+    await FlutterDisplayMode.setHighRefreshRate();
+  } catch (_) {}
   runApp(const LoveMobileApp());
 }
